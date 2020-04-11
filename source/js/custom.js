3cda5fe8a39b586c05570427b58b4529368b6349
YunYouJun <me@yunyoujun.cn>
2020-04-11 17:11:24 +0800

    :pencil: update content

--- source/js/custom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const pattern = Trianglify({
-    palette: ["YlGnBu", "GnBu", "Purples", "Blues"],
-  });
-  const canvas = document.createElement("canvas");
-  canvas.classList.add("trianglify-bg");
-  document.body.appendChild(pattern.canvas(canvas));
-});
